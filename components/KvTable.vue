ae4002f16a2eb4b9f678e900d298b3945b506ee4
Outvi V <git@outv.im>
2021-03-13 22:41:34 +0800

    feat(KvTable): nonQVL

--- components/KvTable.vue
+++ components/KvTable.vue
@@ -5,12 +5,19 @@
     </b-table-column>
 
     <b-table-column v-slot="props" field="value" centered>
-      <div v-if="props.row.value == 't'" class="tableUnit colorYes">是</div>
-      <div v-else-if="props.row.value == 'f'" class="tableUnit colorNo">否</div>
-      <div v-else-if="props.row.value == 'p'" class="tableUnit colorPartial">
-        部分
-      </div>
-      <div v-else class="tableUnit colorUnknown">未知</div>
+      <template v-if="props.row.nonQVL">
+        {{ props.row.value }}
+      </template>
+      <template v-else>
+        <div v-if="props.row.value == 't'" class="tableUnit colorYes">是</div>
+        <div v-else-if="props.row.value == 'f'" class="tableUnit colorNo">
+          否
+        </div>
+        <div v-else-if="props.row.value == 'p'" class="tableUnit colorPartial">
+          部分
+        </div>
+        <div v-else class="tableUnit colorUnknown">未知</div>
+      </template>
     </b-table-column>
   </b-table>
 </template>
@@ -22,7 +29,7 @@ import { Component, Prop } from 'vue-property-decorator'
 @Component
 export default class Infobox extends Vue {
   @Prop()
-  table!: { key: string, value: string }[]
+  table!: { key: string, value: string, nonQVL?: boolean }[]
 }
 </script>
 
