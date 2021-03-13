e568be8a1fb2a0aa19f0350ef9c819c8f69507e1
Outvi V <git@outv.im>
2021-03-13 19:39:06 +0800

    fix: rename to t/f/p/u

--- types.ts
+++ types.ts
@@ -1,10 +1,10 @@
 type GamePlatform = 'android' | 'ios'
 
 export type QVLResponse = 't' | 'f' | 'p' | 'u'
-export type QVLTruey = true | '+'
-export type QVLFalsy = false | '-'
-export type QVLPartial = 'partial' | '*'
-export type QVLUnknown = 'unknown' | null | undefined | '?'
+export type QVLTruey = true | 't'
+export type QVLFalsy = false | 'f'
+export type QVLPartial = 'partial' | 'p'
+export type QVLUnknown = 'unknown' | null | undefined | 'u'
 
 export type QuadValuedLogic = QVLTruey | QVLFalsy | QVLPartial | QVLUnknown
 export interface GameEntry {
--- utils/tr.ts
+++ utils/tr.ts
@@ -1,8 +1,8 @@
 import type { QuadValuedLogic, QVLResponse } from '@/types'
 
 export function analyzeQVL(text: QuadValuedLogic): QVLResponse {
-  if (text === true || text === '+') return 't'
-  if (text === false || text === '-') return 'f'
-  if (text === 'partial' || text === '*') return 'p'
+  if (text === true || text === 't') return 't'
+  if (text === false || text === 'f') return 'f'
+  if (text === 'partial' || text === 'p') return 'p'
   return 'u'
 }
